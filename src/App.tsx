88c959396965721a82ac5879f3139bc40877c5a3
recode <tnrecode@gmail.com>
2026-01-31 01:12:55 +0500

    feat: init repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/shared/ui/button';
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button variant={'destructive'}>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
